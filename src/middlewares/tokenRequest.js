2b5b880f8a80056d122261143a1f2b500ac9fe2b
Fabiana Ribas <fabianalv.ribas@gmail.com>
2020-10-15 16:22:38 -0300

    Aplicando modificaçoes exigidas pelo ESLint
    Signed-off-by: Fabiana Ribas <fabianalv.ribas@gmail.com>

--- src/middlewares/tokenRequest.js
+++ src/middlewares/tokenRequest.js
@@ -1,8 +1,8 @@
 module.exports = (req, res, next) => {
   console.log(`MEHTOD${req.method}`);
   if (
-    req.method == 'GET' ||
-    (req.method != 'GET' && req.headers.authorization == 'pix')
+    req.method === 'GET' ||
+    (req.method !== 'GET' && req.headers.authorization === 'pix')
   ) {
     console.log(req);
     next();
