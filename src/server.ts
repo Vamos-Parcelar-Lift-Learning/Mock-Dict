d6f2540d612cf4a6d0d87d7db86b549f267d20fc
mrodrigues <matheus.1nascimento58@gmail.com>
2020-10-16 15:07:18 -0300

    corrigindo porta de acesso

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import authMiddleware from './middlewares/authMiddleware';
 dotenv.config();
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.use(jsonServer.bodyParser);
 server.use(authMiddleware);
