ddd9d534f7c7ab79c9bf53b58b516edc037a17e2
matheus456 <matheus456456@gmail.com>
2020-10-21 16:08:15 -0300

    Configura porta

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import middlewares from './middlewares';
 dotenv.config();
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.use(jsonServer.bodyParser);
 server.use(jsonServer.rewriter(routes));
